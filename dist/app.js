2146da511c7b0c8a42f8a7bd549771a5512bb579
dario-nihil <dario.nihil@gmail.com>
2022-11-27 18:31:23 +0100

    installed express and types definition to support typescript

---
+++ dist/app.js
@@ -0,0 +1,11 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.listen(8080, '127.0.0.1', () => {
+    console.log('Server app & running....');
+    console.log('Listening on port 8080');
+});
---
+++ src/app.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+
+const app = express();
+
+app.listen(8080, '127.0.0.1', () => {
+  console.log('Server app & running....');
+  console.log('Listening on port 8080');
+});
